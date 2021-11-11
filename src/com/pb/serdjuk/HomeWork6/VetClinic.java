43d3bc48feadf394498ff746252e9fbd43934eea
Mikhail Serdjuk <mikhail.serdjuk@privatbank.ua>
2021-11-11 23:31:06 +0200

    init commit

--- src/com/pb/serdjuk/HomeWork6/VetClinic.java
+++ src/com/pb/serdjuk/HomeWork6/VetClinic.java
@@ -9,23 +9,27 @@ public class VetClinic {
         cat.setFood("Молоко");
         cat.setLocation("Комнате");
 
-         Cat cat2 = new Cat("Рыжик", "рыжий");
-         cat2.setFood("Молоко и мясо");
-         cat2.setLocation("Улица");
+        Cat cat2 = new Cat("Рыжик", "рыжий");
+        cat2.setFood("Молоко и мясо");
+        cat2.setLocation("Селе");
 
         Horse horse = new Horse("Ракета",88);
         horse.setFood("Сено");
         horse.setLocation("Конюшне");
 
-        Dog dog = new Dog("Отто", "Немецкий");
+        Dog dog = new Dog("Отто", "Немецкий Дог");
         dog.setFood("Мясо");
         dog.setLocation("Вольере во дворе дома");
+
+        Dog dog2 = new Dog("Му-Му", "Дворняга");
+        dog2.setFood("Мясо и кости");
+        dog2.setLocation("у Немого");
 
 
          Class clazzCat = Cat.class;
          Class clazzDog = Dog.class;
 
-         Animal[] animals = new Animal[] {cat, horse, dog};
+         Animal[] animals = new Animal[] {cat, cat2, horse, dog, dog2};
 
          Class clazz = Class.forName("com.pb.serdjuk.HomeWork6.Veterinarian");
 
@@ -42,8 +46,8 @@ public class VetClinic {
          }
 
          //Veterinarian.class.newInstance().treatAnimal(cat);
-         Class catClazz = Cat.class;
-         Class catClazz2 = cat2.getClass();
+         /*Class catClazz = Cat.class;
+         Class catClazz2 = cat2.getClass();*/
      }
 }
 
